55556f49a70a13e276817f3f38a6369a49a2775c
Jonathan Lubaway <NeoJax@gmail.com>
2017-10-12 07:28:02 -0700

    Finished part-3 will come back to double-check everthing

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -1,5 +1,6 @@
 $(document).ready(() => {
   let count = /\d/.exec($('#cart-item-count').text());
+  let total = parseFloat($('#total-cost').text().slice(1));
   $('#cart-button').click(function() {
     $('#cartModal').show();
     $('.cart').show();
@@ -7,10 +8,18 @@ $(document).ready(() => {
   $('.add-cart-btn').click(function() {
     count++;
     $('#cart-item-count').text(`(${count})`);
-    $('.cartItems').append(`${$(this).siblings('.item-name').text()} ${$(this).siblings('.item-price').text()}<br>`);
+    $('.cartItems').append(`${$(this).siblings('.item-name').text()}
+     <span class="cartItemCost">${$(this).siblings('.item-price').text()}
+     </span><br>`);
+     total += parseFloat($(this).siblings('.item-price').text().slice(1));
+     $('#total-cost').text(`$${total}`);
   });
-  $('#cartModal').click((e) => {
-    if (e.target.id === 'cartModal') {
+  $('#closeModal').click(function() {
+    $('#cartModal').css('display', 'none');
+    $('.cart').hide();
+  });
+  $('#cartModal').click(function(event) {
+    if (event.target.id === 'cartModal') {
       $('#cartModal').css('display', 'none');
       $('.cart').hide();
     }
